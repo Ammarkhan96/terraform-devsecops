e6017140e7010701994906da6258718af2853e80
Ammarkhan96 <ammarkhaan96@gmail.com>
2026-09-18 17:55:24 +0500

    edit tf files

--- main.tf
+++ main.tf
@@ -24,13 +24,3 @@ resource "aws_s3_bucket_public_access_block" "app_bucket" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-output "s3_bucket_name" {
-  description = "Created S3 bucket name"
-  value       = aws_s3_bucket.app_bucket.bucket
-}
-
-output "s3_bucket_arn" {
-  description = "Created S3 bucket ARN"
-  value       = aws_s3_bucket.app_bucket.arn
-}
