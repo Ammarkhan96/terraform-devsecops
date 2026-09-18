885067bb333a9de7a7803aeeb88c9b9aa0178146
Ammarkhan96 <ammarkhaan96@gmail.com>
2026-09-18 17:52:02 +0500

    edit main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-terraform {
-  required_version = ">= 1.6.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "eu-north-1"
-}
-
-variable "bucket_name" {
-  description = "Unique S3 Bucket name"
-  type        = string
-}
-
 resource "aws_s3_bucket" "app_bucket" {
   bucket = var.bucket_name
 
