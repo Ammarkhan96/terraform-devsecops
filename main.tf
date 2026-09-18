91917ca37fcd14af39e39dcb77b07c68bb3e3656
Ammarkhan96 <ammarkhaan96@gmail.com>
2026-09-18 17:47:53 +0500

    fix: update Checkov security scan

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "bucket_name" {
+  description = "Unique S3 Bucket name"
+  type        = string
+}
+
 resource "aws_s3_bucket" "app_bucket" {
   bucket = var.bucket_name
 
@@ -23,4 +49,14 @@ resource "aws_s3_bucket_public_access_block" "app_bucket" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+output "s3_bucket_name" {
+  description = "Created S3 bucket name"
+  value       = aws_s3_bucket.app_bucket.bucket
+}
+
+output "s3_bucket_arn" {
+  description = "Created S3 bucket ARN"
+  value       = aws_s3_bucket.app_bucket.arn
 }
